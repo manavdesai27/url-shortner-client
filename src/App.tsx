bbc80e5f49eafb33899e5384adbf516734620de2
manavdesai27 <manav27202@gmail.com>
2026-03-01 16:00:46 +0530

    Add vercel analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import './App.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { Analytics } from '@vercel/analytics/react';
 
 import Header from './components/Header'
 import Hero from './components/Hero'
@@ -9,14 +10,17 @@ import Signup from './pages/Signup'
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<><Hero /><Main /></>} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <Analytics />
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<><Hero /><Main /></>} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </BrowserRouter>
+    </>
   )
 }
 
